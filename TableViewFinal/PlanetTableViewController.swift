f2f653be9da735068005a34ef4440dffdcbe3f86
JoyceMatos <j.matosjoyce@gmail.com>
2016-10-10 14:16:35 -0400

    All tests passing

--- TableViewFinal/PlanetTableViewController.swift
+++ TableViewFinal/PlanetTableViewController.swift
@@ -15,7 +15,7 @@ class PlanetTableViewController: UITableViewController {
     
     func generatePlanetInfo() {
         let earth: Planet = Planet(name: "Earth", numberOfMoons: 1, fullOrbit: 365.26, facts: ["Earth is the only planet not named after a god.", "Earth has a powerful magnetic field.", "Earth was once believed to be the center of the universe."])
-        let mars: Planet = Planet(name: "mars", numberOfMoons: 2, fullOrbit: 687.0, facts: ["Mars and Earth have approximately the same landmass.", "Mars is home to the tallest mountain in the solar system.", "Pieces of Mars have fallen to Earth."])
+        let mars: Planet = Planet(name: "Mars", numberOfMoons: 2, fullOrbit: 687.0, facts: ["Mars and Earth have approximately the same landmass.", "Mars is home to the tallest mountain in the solar system.", "Pieces of Mars have fallen to Earth."])
         self.planets.append(earth)
         self.planets.append(mars)
     }
